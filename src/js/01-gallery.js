d34300641c1f782c06a87f717834a1c878bc4032
Baltakov <tira2500@gmail.com>
2023-11-17 19:22:17 +0200

    gallery, library SimpleLightBox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+    `
+    )
+    .join('');
+}
+
+gallery.innerHTML = createMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
