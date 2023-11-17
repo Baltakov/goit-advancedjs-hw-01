bc989d9bd5834e8e5dcab9ec857e017193e796e5
Baltakov <tira2500@gmail.com>
2023-11-17 19:57:10 +0200

    videoplayer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+// 02-video.js
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const vimeoPlayer = new Player(document.getElementById('vimeo-player'));
+
+const localStorageKey = 'videoplayer-current-time';
+
+vimeoPlayer.on(
+  'timeupdate',
+  throttle(async function (event) {
+    const time = await vimeoPlayer.getCurrentTime();
+    localStorage.setItem(localStorageKey, time);
+  }, 1000)
+); // Оновлення часу не частіше ніж раз на секунду
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const savedTime = localStorage.getItem(localStorageKey);
+  if (savedTime) {
+    await vimeoPlayer.setCurrentTime(savedTime);
+  }
+});
